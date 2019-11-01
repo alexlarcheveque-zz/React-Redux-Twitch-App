7728856663995357a1ee3d5172cbefade699de44
Alex Larcheveque <larchevequealex@gmail.com>
2019-10-31 20:54:12 -0700

    Added neccessary routes to the Application. Created navigation bar header.

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import StreamCreate from "./StreamCreate";
+import StreamDelete from "./StreamDelete";
+import StreamEdit from "./StreamEdit";
+import StreamList from "./StreamList";
+import StreamShow from "./StreamShow";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
